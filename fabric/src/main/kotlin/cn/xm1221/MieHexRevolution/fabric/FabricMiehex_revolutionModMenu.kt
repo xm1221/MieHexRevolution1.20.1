5091329339a0420df72549c7ecd5206a32cd134d
xm1221 <3850632202@qq.com>
2026-07-26 10:34:49 +0800

    init

--- fabric/src/main/kotlin/cn/xm1221/MieHexRevolution/fabric/FabricMiehex_revolutionModMenu.kt
+++
@@ -1,9 +0,0 @@
-package cn.xm1221.MieHexRevolution.fabric
-
-import com.terraformersmc.modmenu.api.ConfigScreenFactory
-import com.terraformersmc.modmenu.api.ModMenuApi
-import cn.xm1221.MieHexRevolution.Miehex_revolutionClient
-
-object FabricMiehex_revolutionModMenu : ModMenuApi {
-    override fun getModConfigScreenFactory() = ConfigScreenFactory(Miehex_revolutionClient::getConfigScreen)
-}
--- libs/README.md
+++
@@ -1,1 +0,0 @@
-You can put JAR files here for dependencies which aren't published to CurseForge/Modrinth or available on any Maven.
