6fa5df446390d383d3c96d7bfa1f6381b13a541f
xm1221 <3850632202@qq.com>
2026-07-26 11:03:50 +0800

    init2

---
+++ neoforge/src/main/kotlin/cn/xm1221/MieHexRevolution/neoforge/NeoforgeMiehex_revolution.kt
@@ -0,0 +1,11 @@
+package cn.xm1221.MieHexRevolution.neoforge
+
+import cn.xm1221.MieHexRevolution.Miehex_revolution
+import net.neoforged.fml.common.Mod
+
+@Mod(Miehex_revolution.MODID)
+class NeoforgeMiehex_revolution {
+    init {
+        Miehex_revolution.init()
+    }
+}
